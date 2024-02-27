fcf1984b7a0085ca35c0643cd041c775d5562b6c
crizin <crizin@gmail.com>
2024-02-28 00:09:01 +0900

    Fix Ppomppu markup change

--- src/main/java/me/cozo/api/infrastructure/crawler/PpomppuCrawler.java
+++ src/main/java/me/cozo/api/infrastructure/crawler/PpomppuCrawler.java
@@ -25,14 +25,14 @@ public class PpomppuCrawler extends Crawler {
 
 	@Override
 	protected Set<Article> getArticles(Board board, Document document) {
-		return document.select("#revolution_main_table tbody tr[class^=common-list]").stream()
+		return document.select("#revolution_main_table tbody tr[class=baseList]").stream()
 			.filter(tr -> !tr.html().contains("해당글은 운영자에 의해 블라인드 처리된 글입니다."))
 			.map(tr -> Article.builder()
 				.board(board)
 				.originId(TextUtils.getFragment(attr(tr, "td:eq(2) a", "href"), "no=(\\d+)"))
-				.title(textOnly(tr, "td:eq(2) a font.list_title"))
+				.title(textOnly(tr, "td:eq(2) a span"))
 				.hits(number(tr, "td:eq(5)"))
-				.comments(number(tr, "td:eq(2) span.list_comment2 span"))
+				.comments(number(tr, "td:eq(2) span.baseList-c"))
 				.likes(Optional.of(text(tr, "td:eq(4)"))
 					.filter(StringUtils::isNotBlank)
 					.map(value -> TextUtils.getFragment(value, "^(\\d+)"))
