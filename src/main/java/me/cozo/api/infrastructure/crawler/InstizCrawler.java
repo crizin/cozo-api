35abae5cde0ac19889b1df8de005a2f4c270e6ed
JaeYong Lee <crizin@gmail.com>
2025-07-18 00:39:04 +0900

    Refactor InstizCrawler to use new parsing logic and improve data extraction

--- src/main/java/me/cozo/api/infrastructure/crawler/InstizCrawler.java
+++ src/main/java/me/cozo/api/infrastructure/crawler/InstizCrawler.java
@@ -5,12 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import me.cozo.api.application.crawler.Crawler;
 import me.cozo.api.domain.model.Article;
 import me.cozo.api.domain.model.Board;
+import me.cozo.api.infrastructure.helper.DateUtils;
 import me.cozo.api.infrastructure.helper.TextUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -26,19 +27,23 @@ public class InstizCrawler extends Crawler {
 	@Override
 	protected Set<Article> getArticles(Board board, Document document) {
 		return document.select("#mboard > tbody > tr").stream()
-			.filter(tr -> StringUtils.equals(tr.attr("id"), "detour"))
-			.map(tr -> Article.builder()
-				.board(board)
-				.originId(TextUtils.getFragment(attr(tr, "td.listsubject a", "href"), "/pt/(\\d+)"))
-				.title(textOnly(tr, "td.listsubject a"))
-				.hits(number(tr, "td:eq(4)"))
-				.comments(number(tr, "td.listsubject a.cmt2"))
-				.likes(number(tr, "td:eq(5)"))
-				.containsImage(has(tr, "td.listsubject i.fa-image"))
-				.containsVideo(has(tr, "td.listsubject i.fa-youtube"))
-				.createdAt(time(tr, "td.regdate"))
-				.build()
-			)
+			.filter(tr -> has(tr, ".listno"))
+			.map(tr -> {
+				var info = Objects.requireNonNull(tr.selectFirst(".listno")).text();
+
+				return Article.builder()
+					.board(board)
+					.originId(TextUtils.getFragment(attr(tr, "td.listsubject a", "href"), "/pt/(\\d+)"))
+					.title(textOnly(tr, "td.listsubject .sbj"))
+					.hits(TextUtils.getFragmentOptional(info, "조회 (\\d+)").map(TextUtils::number).orElse(0))
+					.comments(number(tr, "td.listsubject a .cmt2"))
+					.likes(TextUtils.getFragmentOptional(info, "추천 (\\d+)").map(TextUtils::number).orElse(0))
+					.containsImage(has(tr, "td.listsubject i.fa-image"))
+					.containsVideo(has(tr, "td.listsubject i.fa-youtube"))
+					.createdAt(TextUtils.getFragmentOptional(info, "^([^l]+)").map(String::strip).map(DateUtils::parse).orElse(null))
+					.build();
+			})
+			.filter(Objects::nonNull)
 			.collect(Collectors.toSet());
 	}
 
