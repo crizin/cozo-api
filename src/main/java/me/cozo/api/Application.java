a15f46f458cfe5ecaed98166f09ecbd5796cf84a
JaeYong Lee <crizin@gmail.com>
2025-10-10 21:00:35 +0900

    Fix Application main method visibility and refactor build workflow: Change `static` to `public static` in `Application` for proper startup and update Gradle setup in GitHub Actions for better clarity and separation.

--- src/main/java/me/cozo/api/Application.java
+++ src/main/java/me/cozo/api/Application.java
@@ -8,7 +8,7 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class Application {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(Application.class, args);
 	}
